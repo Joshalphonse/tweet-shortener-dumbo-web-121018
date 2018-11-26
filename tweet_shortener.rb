3095b0853e33d66acc2112cbc7855d9181620859
Joshua Alphonse <joshalphonse@gmail.com>
2018-11-26 06:38:50 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def dictionary
+  {
+    "hello" => 'hi',
+    "Hello" =>' Hi',
+    "to" => '2',
+    "too" => '2',
+    "two" => '2',
+    "To" => '2',
+    "Too" => '2',
+    "Two" => '2',
+    "for" => '4',
+    "four" => '4',
+    "For" => '4',
+    "Four" => '4',
+    "be" => 'b',
+    "you" => 'u',
+    "Be" => 'B',
+    "You" => 'U',
+    "at" => '@',
+    "and" => '&'
+  }
+end
